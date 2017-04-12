cb54e26d321380ebf8e71c5a7f675361edd6a351
liyatang <liyatang@xiaonongnv.com>
2017-04-12 15:13:51 +0800

    c

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function sortByRank(list) {
     if (!list) {
         return list;
     }
-    const arr = _.partition(list, v => !v.rank);
+    const arr = _.partition(list, v => v.rank);
     return _.sortBy(arr[0], v => -v.rank).concat(arr[1]);
 }
 
