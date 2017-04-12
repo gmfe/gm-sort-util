495c506876742d14702f18cb909bb4be72b56713
liyatang <liyatang@xiaonongnv.com>
2017-04-12 14:34:16 +0800

    c

---
+++ index.js
@@ -0,0 +1,13 @@
+import _ from 'lodash'
+
+function sortByRank(list) {
+    if (!list) {
+        return list;
+    }
+    const arr = _.partition(list, v => !v.rank);
+    return _.sortBy(arr[0], v => -v.rank).concat(arr[1]);
+}
+
+module.exports = {
+    sortByRank
+};
