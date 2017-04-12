364c3a05b475c473e2bbcb09c57a4ba2a00e8add
liyatang <liyatang@xiaonongnv.com>
2017-04-12 16:44:03 +0800

    c

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import _ from 'lodash'
 
+// 一级分类 二级分类  SPU 根据rank排序
 function sortByRank(list) {
     if (!list) {
         return list;
@@ -8,6 +9,16 @@ function sortByRank(list) {
     return _.sortBy(arr[0], v => -v.rank).concat(arr[1]);
 }
 
+// 优先排营销分类， 之后一级分类 二级分类  SPU 根据rank排序
+function sortByMarketingThenRank(list){
+    if(!list){
+        return list;
+    }
+    const arr = _.partition(list, v => v.id[0] === 'E');
+    return sortByRank(arr[0]).concat(arr[1]);
+}
+
 module.exports = {
-    sortByRank
+    sortByRank,
+    sortByMarketingThenRank
 };
